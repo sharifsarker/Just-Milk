c0fbd6b33ac5a664acbc761c85806cf3364ee5f4
sharifsarker <sharifsarker683@gmail.com>
2020-10-31 13:30:43 +0600

    Initial Commit

---
+++ Just Milk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Just Milk
+//
+//  Created by Sharif Sarker on 31/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
