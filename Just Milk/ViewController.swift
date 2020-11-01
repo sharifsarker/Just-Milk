76f975e08fd3f527b6ab7cd6fdc2ee2e9204ebd6
sharifsarker <sharifsarker683@gmail.com>
2020-11-01 10:39:59 +0600

    view did load disappear function added

--- Just Milk/ViewController.swift
+++ Just Milk/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    override func viewDidDisappear(_ animated: Bool) {
+        <#code#>
+    }
 }
 
